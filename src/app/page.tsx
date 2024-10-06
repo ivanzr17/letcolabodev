25b145f3a3efeb09138f14f41611012707b16617
Ivanzr17 <ivanzafrarendal@gmail.com>
2024-10-06 20:10:36 +0200

    Add mongoose connect to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,40 +2,19 @@ import { addData, PostModel } from "@/models/Post";
 import Hero from "./components/Hero";
 import Project from "./components/Project";
 import { getUser } from "@workos-inc/authkit-nextjs";
+import mongoose from "mongoose";
 
-interface SearchParams {
-  page?: string;
-}
-
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: SearchParams;
-}) {
+export default async function Home() {
   const { user } = await getUser();
-
-  const page = parseInt(searchParams.page ?? "1") || 1;
-  const limit = 5;
-  const skip = (page - 1) * limit;
-
-  const [posts, totalPosts] = await Promise.all([
-    PostModel.find({}, {}, { skip, limit, sort: "-createdAt" }),
-    PostModel.countDocuments(),
-  ]);
-
-  const totalPages = Math.ceil(totalPosts / limit);
-
-  const projectsWithUser = await addData(posts, user);
-
+  await mongoose.connect(process.env.MONGO_URI as string);
+  const post = await addData(
+    await PostModel.find({}, {}, { limit: 5, sort: "-createdAt" }),
+    user
+  );
   return (
     <>
       <Hero />
-      <Project
-        header=""
-        projects={projectsWithUser}
-        page={page}
-        totalPages={totalPages}
-      />
+      <Project header="" projects={post} />
     </>
   );
 }
